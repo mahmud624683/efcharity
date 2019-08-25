da61bc83592ed19696c03287e0e0e2ee364157dd
mahmud624683 <mhb1406077@gmail.com>
2019-08-26 01:37:25 +0600

    25/8/19

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var slideIndex = 0;
 showSlides();
-
+/*
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -22,17 +22,42 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
-}
+}*/
 
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none"; 
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1} 
   slides[slideIndex-1].style.display = "block"; 
+  
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+
   setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+
+
+
+//team
+filterSelection("all") // Execute the function and show all columns
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
 }
 
